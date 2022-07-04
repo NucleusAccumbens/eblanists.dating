66c8de56b69390d34345b2e3933af54aa7972557
nucleus accumbens <bezlisa@mail.ru>
2022-07-04 12:55:02 +0400

    LoggingBehaviour

---
+++ Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviours;
+
+public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+{
+    private readonly ILogger _logger;
+    private readonly ICurrentUserService _currentUserService;
+
+    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+    {
+        _logger = logger;
+        _currentUserService = currentUserService;
+    }
+
+    public Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+        var telegramUserId = _currentUserService.TelegramUserChatId ?? default;
+        var datingAppUserId = _currentUserService.DatingAppUserId ?? default;
+
+        _logger.LogInformation("eblanist.dating request: {Name} {@TelegramUserChatId} {@UserId}  {@Request}",
+            requestName, telegramUserId, datingAppUserId, request);
+
+        return Task.CompletedTask;
+    }
+}
+
